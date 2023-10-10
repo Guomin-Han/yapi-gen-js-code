aaa1076ff77a24ae822f0194550b9e6e76f713a8
Guomin Han <gm@MacBook-Pro.local>
2023-10-10 14:46:42 +0800

    fix: 修复readlineSync提示消息乱码问题

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const readlineSync = require('readline-sync');
 
-
 module.exports = {
   globalCode: `import request from '@/utils/request' \n`,
   categoryId: null,
@@ -11,7 +10,8 @@ module.exports = {
       return p.replace(/[^a-zA-Z0-9\_]+/g, '')
     })
     if (!apipaths.length) {
-      return readlineSync.question('由于该api路径为根路径, 请输入请求方法名称:\n').replace(/[^a-zA-Z0-9\_]+/g, '')
+      console.log(`由于该api路径${apipath}为根路径, 请输入请求方法名称:`);
+      return readlineSync.question().replace(/[^a-zA-Z0-9\_]+/g, '')
     }
     return categoryId ? apipaths[length - 1] : apipaths.join('_')
   },
